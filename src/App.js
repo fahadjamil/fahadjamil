e57f06101049dff9955b0f6d7b0a11e2861a5abb
fahadjamil <fahadjamil221@gmail.com>
2023-04-04 16:11:26 +0500

    remove unwanted data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component, Suspense } from 'react';
-import { useTranslation, withTranslation, Trans } from 'react-i18next';
-import logo from './logo.svg';
 import './App.css';
 import { Topheader } from './components/Topheader';
 
